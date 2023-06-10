60a59d95a30d2faf1836683501610f9a0cfbd79b
seulgi <guseul9405@gmail.com>
2023-06-10 10:27:12 +0900

    폰트 사이즈 변경

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MyLoading = styled.div`
-  width: 300px;
-  height: 100px;
-  margin: auto;
+  width: 500px;
+  height: 150px;
+  margin: 100px auto;
   line-height: 100px;
   text-align: center;
+  font-size: 70px;
+  font-weight: 600;
 `;
 
 export default function Loading() {
